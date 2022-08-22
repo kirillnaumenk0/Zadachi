5d34d57035301f2d48227c55d5f381c9cfd9d610
Kirill <kirillnaumenk0@mail.ru>
2022-08-22 14:28:48 +0600

    commit end

---
+++ №2/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+
+    static int[] CreaterArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1, 15);
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array)
+    {
+        Console.WriteLine("Вывод массива");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+    static void rar(int[] array)
+    {
+        int j = 1;
+        for (int i = 0; i < array.Length/2; i++)
+        {
+            int res = array[i] * array[array.Length - j];
+            Console.Write(" ");
+            Console.Write(res);
+            j++;
+        }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] array = CreaterArray(8);
+        WriteArray(array);
+       Console.Write(" - ");
+        rar(array);
+    }
+}
