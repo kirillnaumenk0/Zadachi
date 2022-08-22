ace06c2f3b02a9209c517de5a061e08fe93e7b80
Kirill <kirillnaumenk0@mail.ru>
2022-08-22 13:51:26 +0600

    commit1

---
+++ №1/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+
+    static int[] CreaterArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1, 300);
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array)
+    {
+        Console.WriteLine("Вывод массива");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+
+    static void MultipleTwoNumber(int[] array)
+    {
+        int s = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 10)
+            {
+                if (array[i] < 99)
+
+                {
+                    s++;
+                }
+            }
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine("Cумма элементов, стоящих на  позициях от 10 до 99: " + s);
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] array = CreaterArray(123);
+        WriteArray(array);
+        MultipleTwoNumber(array);
+    }
+}
